0b73256ea9152bdd0b76bbdefd32668127fd907c
bennedii <155365185+bennedii@users.noreply.github.com>
2025-01-16 00:29:45 +0300

    initial

---
+++ src/main/java/com/example/Congratulator/CongratulatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.Congratulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CongratulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CongratulatorApplication.class, args);
+	}
+
+}
